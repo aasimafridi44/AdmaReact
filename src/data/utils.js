1dfcd7bae64ef05ae96419e6278e44081d9de93e
Aasim Khan <aasim.afridi107400@gmail.com>
2023-10-31 11:48:43 +0530

    Make dynamice proxyserver url

--- src/data/utils.js
+++ src/data/utils.js
@@ -1,5 +1,15 @@
-const proxyServier = ['http://localhost:8080/', 'https://cors-anywhere.herokuapp.com/']
-export const endPoint = proxyServier[0] + 'https://adma.farmbeats.azure.net';
+export const requestedUrl = (requestUrl) => {
+  const proxyServer = ['http://localhost:8080/', 'https://cors-anywhere.herokuapp.com/']
+  let proxyUrl
+  if (requestUrl.indexOf('localhost') !== -1) {
+    proxyUrl = proxyServer[0];
+  } else {
+    proxyUrl = proxyServer[1];
+  }
+  return proxyUrl
+}
+
+export const endPoint = requestedUrl(window.location.href) + 'https://adma.farmbeats.azure.net';
 export const apiToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IjlHbW55RlBraGMzaE91UjIybXZTdmduTG83WSIsImtpZCI6IjlHbW55RlBraGMzaE91UjIybXZTdmduTG83WSJ9.eyJhdWQiOiJodHRwczovL2Zhcm1iZWF0cy5henVyZS5uZXQiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC9jNmMxZTlkYS01ZDBjLTRmOGYtOWEwMi0zYzY3MjA2ZWZiZDYvIiwiaWF0IjoxNjk4NzMxMDA3LCJuYmYiOjE2OTg3MzEwMDcsImV4cCI6MTY5ODczNDkwNywiYWlvIjoiRTJGZ1lGQm9tZk5hNzFnMEsyTXN3elBGR2hWREFBPT0iLCJhcHBpZCI6IjNiZDYzZDQwLTIyYzMtNDQ2ZS1iOGI3LTgwOGMxNTdkMTFkOCIsImFwcGlkYWNyIjoiMSIsImlkcCI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0L2M2YzFlOWRhLTVkMGMtNGY4Zi05YTAyLTNjNjcyMDZlZmJkNi8iLCJvaWQiOiJjMTg1Y2ZjMy0xNTE5LTQzOWQtOTBlNS0zMzM3MmUzNjIxMjEiLCJyaCI6IjAuQVVrQTJ1bkJ4Z3hkajAtYUFqeG5JRzc3MXNXVHVMRWNyb3RCbUpNTGVNQkNKYlJKQUFBLiIsInN1YiI6ImMxODVjZmMzLTE1MTktNDM5ZC05MGU1LTMzMzcyZTM2MjEyMSIsInRpZCI6ImM2YzFlOWRhLTVkMGMtNGY4Zi05YTAyLTNjNjcyMDZlZmJkNiIsInV0aSI6IjNLdVk5bzZVWUVTVTdwcklOQnBmQUEiLCJ2ZXIiOiIxLjAifQ.Fo-n2n3R7W4ejA1TTDOGgWpUQWln-reYoXwX-Q2qihJtJMd-sAfD0wsmc1LsK0MHQ5I2S1pYQEgMAXTBtdYnBkjGNykm10V7arRD0HoUdTQ_LnHjZkF0ngKbTBklKdu_DQJO7RZkyvEvOY_UIBbprcpbnDFXp_PEVR22FgnXf6sR7cONIqsoMXiLmNUdZTs_U4bgbzdYSzxbzvZlFkos21PWn6NzX71Iuqb9S1I4_QOI_XWO38khDzt2LXFhmf_iHgUSD2hLOZGSN7CzcZ5aHp4Hrv0FQtiytaW3eI4MwfPMjT0tF6p8slsdaSldOag2Fk6LMUkmolHWAXeprQT2LA"
 // Define your custom headers
 export const headers = {
@@ -30,11 +40,11 @@ export const convertCoordinatesToLatLngArray = (nestedCoordinatesArray) => {
       return latLngArray.filter((latLng) => latLng !== null);
   }
 
-    export const resultArray = (inputArray) => {
-        return inputArray.map(obj => {
-            // Destructure the lat and lng properties and rename them
-            const { lat, lng, ...rest } = obj; // Remove lat and lng properties
-            const [newLat, newLng] = [lat, lng].map(val => val); // Get absolute values
-            return [newLat, newLng, ...Object.values(rest)]; // Add other properties back
-        })
-    };
+export const resultArray = (inputArray) => {
+    return inputArray.map(obj => {
+        // Destructure the lat and lng properties and rename them
+        const { lat, lng, ...rest } = obj; // Remove lat and lng properties
+        const [newLat, newLng] = [lat, lng].map(val => val); // Get absolute values
+        return [newLat, newLng, ...Object.values(rest)]; // Add other properties back
+    })
+};
