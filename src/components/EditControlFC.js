82f7535c7c17c3bf6a1ec73b307cdecc7ee81612
Aasim Khan <aasim.afridi107400@gmail.com>
2023-11-21 22:32:20 +0530

    Fix image issue

--- src/components/EditControlFC.js
+++ src/components/EditControlFC.js
@@ -53,7 +53,7 @@ export default function EditControlFC({ geojson, setGeojson, onBoundarySave, sat
   //[[geojson?.features[0]?.geometry?.coordinates]]
   // [ [ -3.93012, -39.198528 ], [ -3.880602, -39.093907 ] ]
   //""https://tavant-my.sharepoint.com/personal/aasim_khan_tavant_com/Documents/aasim-field-8.png"
-  //console.log('cords bound', getBoundsCords(geojson?.features[0]?.geometry?.coordinates))
+  console.log('cords bound', getBoundsCords(geojson?.features[0]?.geometry?.coordinates))
   const boundCords = getBoundsCords(geojson?.features[0]?.geometry?.coordinates)
   //console.log('length of boundCords ', boundCords)
   return (
@@ -79,7 +79,7 @@ export default function EditControlFC({ geojson, setGeojson, onBoundarySave, sat
         }}
       />
     </FeatureGroup>
-    {!isEdit && satelliteImage && boundCords.length>0 && 
+    {!isEdit && satelliteImage && (boundCords.length !==0 ) && 
       <>
       {console.log('satelliteImage', satelliteImage)}
       <ImageOverlay
