893e49501035790f15b4f8093bf45ba7705e2815
Chetan Sharma <chetan2309@gmail.com>
2023-12-01 17:20:29 +0530

    Re-wrote service to show different colors

--- src/service/RasterCalcService.js
+++ src/service/RasterCalcService.js
@@ -1,122 +1,143 @@
-const legends= [];
-const rasterColorArray = ['#1B8819', '#2ADE08', '#9BFF0C', '#FCE51C', '#F8850C', '#F80711', '#930A0C']
-
+const legends = [];
+const rasterColorArray = ['#1B8819', '#2ADE08', '#9BFF0C', '#FCE51C', '#F8850C', '#F80711', '#930A0C'];
 
 export const calculateValuesForLegend = (avgValue, georasterArr) => {
-    const arrayBefore = [];
-    const arrayAfter = [];
-    let sumBefore = 0;
-    let sumAfter = 0;
+  if (georasterArr.length === 0) {
+    legends[legends.length - 1] = 0;
+    return [{'range': 'N/A', 'color': rasterColorArray[legends.length - 1]}];
+  }
 
-    if (georasterArr.length === 0) {
-      legends[3] = 0;
-      return [{'range': 'N/A' , 'color': rasterColorArray[3]}];
+  let minVal = georasterArr[0];
+  let maxVal = georasterArr[0];
+
+  for (let i = 1; i < georasterArr.length; i++) {
+    const currentValue = georasterArr[i];
+
+    if (currentValue < minVal) {
+      minVal = currentValue;
     }
-    georasterArr.forEach((element) => {
-      if (element < avgValue) {
-        arrayBefore.push(element);
-        sumBefore = sumBefore + element;
-      } else {
-        arrayAfter.push(element);
-        sumAfter = sumAfter + element;
-      }
-    });
-    const avgBefore = isNaN(sumBefore / arrayBefore.length) ? 0 : Math.ceil(sumBefore / arrayBefore.length);
-    const avgAfter = isNaN(sumAfter / arrayAfter.length) ? 0 : Math.ceil(sumAfter / arrayAfter.length);
-  
-    // lower band
-    let band = avgValue - avgBefore; // before value assigned to band
-    if ((band > avgAfter - avgValue )) {
-      band = avgAfter - avgValue;
+
+    if (currentValue > maxVal) {
+      maxVal = currentValue;
     }
-  
-    /**Do not remove this commented code */
-  
-    // higher band
-  /*if ((band > avgAfter - avgValue )) {
-      band = band;
-    } */
-  
-     /* higher - lower band */
-    /* let band = avgAfter - avgBefore; */
-  
-    band = band < 1 ? 1 : band;
-    return getLegends(avgValue, band);
-}
-  
-  export const getLegends = (avgValue, band) => {
-    const average = band / 2;
-    const averageRangeHigher = Math.ceil(avgValue + average);
-    const averageLowerValue = Math.ceil(avgValue - average);
-    const middleBandNumber = 3;
-    const legendArray = [];
-    let stopLower = false;
-    legends[middleBandNumber - 1] = averageRangeHigher;
-    if (averageLowerValue <= 0) {
-      stopLower = true;
-      legendArray[middleBandNumber] = {
-        'range' : '< ' + averageRangeHigher,
-        'color' : rasterColorArray[middleBandNumber]};
-    } else {
-      legends[middleBandNumber] = averageLowerValue;
-      legendArray[middleBandNumber] = {
-        'range' : `${averageLowerValue} - ${averageRangeHigher}`,
-        'color' : rasterColorArray[middleBandNumber]
-      };
-    }
-    return calculateUpperLowerLegends(averageRangeHigher, averageLowerValue, band, legendArray, middleBandNumber, stopLower);
   }
-  
-  export const calculateUpperLowerLegends =  (averageRangeHigher, averageLowerValue, band, legendArray, middleBandNumber, stopLower) => {
-    for (let i = 1; i <= 3; i++) {
-      const legendHigher = Math.ceil(averageRangeHigher + band);
-      legendArray[middleBandNumber - i] = {
-        'range': (i === 3) ? '> ' + averageRangeHigher : `${averageRangeHigher} - ${legendHigher}`,
-        'color': rasterColorArray[middleBandNumber - i]
-        };
-      averageRangeHigher = legendHigher;
-      if (i !== 3) {
-      legends[middleBandNumber - i - 1] = averageRangeHigher;
-      }
-      if (!stopLower) {
-        const legendLower = Math.ceil(averageLowerValue - band);
-        legendArray[middleBandNumber + i] = {
-          'range': ((legendLower <= 0 || (i === 3)) ? '< ' + averageLowerValue :
-            `${legendLower} - ${averageLowerValue}`),
-          'color': rasterColorArray[middleBandNumber + i]
-        };
-        if (legendLower > 0) {
-          averageLowerValue = legendLower;
-          callIfNotEqualToThree(averageLowerValue, middleBandNumber, i);
-        } else {
-            stopLower = true;
-        }
-      }
-    }
-    return legendArray;
+
+  // Adjust the legends based on the length of legends array
+  const quintiles = calculateQuintiles(georasterArr, legends.length);
+  for (let i = 1; i < legends.length; i++) {
+    legends[i] = quintiles[i - 1];
   }
-  
-  export const callIfNotEqualToThree = (averageLowerValue, middleBandNumber, i) => {
+
+  return getLegends(avgValue, minVal, maxVal, georasterArr);
+};
+
+export const calculateQuintiles = (arr, numQuintiles) => {
+  const sortedArr = arr.slice().sort((a, b) => a - b);
+  const quintileValues = [];
+
+  for (let i = 1; i < numQuintiles; i++) {
+    const index = Math.floor((i / numQuintiles) * (sortedArr.length - 1));
+    quintileValues.push(sortedArr[index]);
+  }
+
+  return quintileValues;
+};
+
+export const calculatePercentile = (arr, percentile) => {
+  const sortedArr = arr.slice().sort((a, b) => a - b);
+  const index = Math.floor((percentile / 100) * (sortedArr.length - 1));
+  return sortedArr[index];
+};
+
+export const getLegends = (avgValue, minVal, maxVal, georasterArr) => {
+  const middleBandNumber = 3;
+  const legendArray = [];
+  let stopLower = false;
+
+  legends[middleBandNumber - 1] = calculatePercentile(georasterArr, 80); // Set the highest legend to the 80th percentile
+
+  if (minVal <= legends[1]) {
+    stopLower = true;
+    legendArray[middleBandNumber] = {
+      'range': `< ${legends[2]}`,
+      'color': rasterColorArray[middleBandNumber]
+    };
+  } else {
+    legends[middleBandNumber] = calculatePercentile(georasterArr, 20); // Set the lowest legend to the 20th percentile
+    legendArray[middleBandNumber] = {
+      'range': `${legends[1]} - ${legends[2]}`,
+      'color': rasterColorArray[middleBandNumber]
+    };
+  }
+
+  return calculateUpperLowerLegends(avgValue, calculatePercentile(georasterArr, 80), calculatePercentile(georasterArr, 20), legendArray, middleBandNumber, stopLower);
+};
+
+export const calculateUpperLowerLegends = (avgValue, percentile80, percentile20, legendArray, middleBandNumber, stopLower) => {
+  for (let i = 1; i <= 3; i++) {
+    const legendHigher = Math.ceil(percentile80 + (percentile80 - percentile20) * i);
+    legendArray[middleBandNumber - i] = {
+      'range': (i === 3) ? `> ${percentile80}` : `${percentile80} - ${legendHigher}`,
+      'color': rasterColorArray[middleBandNumber - i]
+    };
+
     if (i !== 3) {
-      legends[middleBandNumber + i] = averageLowerValue;
+      legends[middleBandNumber - i - 1] = legendHigher;
     }
-  }
-  
-  export const calculateColorForRaster = (value) => {
-    let colorV;
-    let index;
-    if (isNaN(value) || value == null || value[0] === 0) {
-      colorV = rasterColorArray[3];
-    } else {
-      legends.forEach((element , i) => {
-        if (!colorV  && value > element) {
-          colorV = rasterColorArray[i];
-        }
-        index = i;
-      });
-      if (!colorV) {
-        colorV =  rasterColorArray[index + 1];
+
+    if (!stopLower) {
+      const legendLower = Math.ceil(percentile20 - (percentile80 - percentile20) * i);
+      legendArray[middleBandNumber + i] = {
+        'range': ((legendLower <= 0 || (i === 3)) ? `< ${percentile20}` : `${legendLower} - ${percentile20}`),
+        'color': rasterColorArray[middleBandNumber + i]
+      };
+
+      if (legendLower > 0) {
+        percentile20 = legendLower;
+        callIfNotEqualToThree(percentile20, middleBandNumber, i);
+      } else {
+        stopLower = true;
       }
     }
-      return colorV;
   }
+
+  // Add an extra legend for values above maxVal
+  legendArray.push({
+    'range': `> ${legends[2]}`,
+    'color': rasterColorArray[rasterColorArray.length - 1] // Use the last color in the array
+  });
+
+  return legendArray;
+};
+
+export const callIfNotEqualToThree = (percentile20, middleBandNumber, i) => {
+  if (i !== 3) {
+    legends[middleBandNumber + i] = percentile20;
+  }
+};
+
+export const calculateColorForRaster = (value) => {
+  if (!isNaN(value) && value !== null && value !== 0) {
+    const n = legends.length;
+
+    const rangeSize = 1 / n;
+    let colorV = rasterColorArray[n - 1]; // Default color for values outside the legends range
+
+    for (let i = 1; i <= n; i++) {
+      const lowerBound = (i - 1) * rangeSize;
+      const upperBound = i * rangeSize;
+
+      if (value >= lowerBound && value < upperBound) {
+        colorV = rasterColorArray[i - 1];
+        // console.log(`Value: ${value}, Range: [${lowerBound}, ${upperBound}), Color: ${colorV}`);
+        break;
+      }
+    }
+
+    // console.log(`Value: ${value}, Final Color: ${colorV}`);
+    return colorV;
+  } else {
+    // console.log(`Invalid value: ${value}, Default Color: ${rasterColorArray[legends.length - 1]}`);
+    return rasterColorArray[legends.length - 1]; // Default color for undefined or null values
+  }
+};
